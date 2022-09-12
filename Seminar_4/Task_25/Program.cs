b892aa282f5d02e4dd56ae5e4b623bc55c45e20d
Alexander <xanderx2201@yandex.ru>
2022-09-12 21:22:29 +0300

    Homework seminar 4 complitte

---
+++ Seminar_4/Task_25/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+using System;
+
+Console.WriteLine("Введите через пробел два числа: A B");
+string numberAB = Console.ReadLine()!;
+string[] partsA = numberAB.Split(' ');
+int numberA = int.Parse(partsA[0]);
+int numberB = int.Parse(partsA[1]);
+int powA = numberA;
+
+for (int i = 2; i <= numberB; i++)
+    powA = powA * numberA; 
+Console.WriteLine($"A в степени B = {powA}");
---
+++ Seminar_4/Task_29/Program.cs
@@ -0,0 +1,35 @@
+﻿// Пользователь вводит 8 чисел. Поместите их в массив и распечатайте.
+using System;
+
+Console.WriteLine("Введите через пробел восемь чисел");
+string numbers = Console.ReadLine()!;
+string[] parts = numbers.Split(' ');  //можно просто распечатать уже этот массив, но мне кажется создать массив чисел более правильно
+int[] array = new int[8];
+
+Complection(parts);
+paint(array);
+
+//for(int i = 0; i < 8; i++){
+//    array[i] = int.Parse(parts[i]);
+//    if (i < 7)
+//        Console.Write($"{array[i]}, ");
+//    else
+//        Console.WriteLine(array[i]);
+//}
+//это простое решение, и я попытался сделать функции
+
+
+int[] Complection(string[] parts){
+    for(int i = 0; i < 8; i++)
+      array[i] = int.Parse(parts[i]);
+    return array;
+}
+
+void paint(int[] array){
+    for(int i = 0; i < 8; i++)
+        if (i < 7)
+            Console.Write($"{array[i]}, ");
+        else
+            Console.WriteLine(array[i]);
+}
+//вроде получилось))
