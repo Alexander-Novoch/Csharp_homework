5cea441f3858bfb62a1acd297dba4d17cb983a48
Alexander <xanderx2201@yandex.ru>
2022-09-25 16:35:19 +0300

    Hoework ask 34 complite

---
+++ Seminar_5/Task_34/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+int size = 0;
+System.Console.WriteLine("Введите размерность массива");
+size = int.Parse(Console.ReadLine()!);
+
+int[] Numbers = new int[size];
+
+Fillarray(Numbers);
+Printarray(Numbers);
+Parity(Numbers);
+
+void Fillarray(int[] Numbers)
+{
+    for (int i = 0; i < size; i++)
+        Numbers[i] = new Random().Next(100, 1000);
+}
+
+void Printarray(int[] Numbers)
+{
+    for (int i = 0; i < size; i++)
+        Console.Write($"{Numbers[i]} ");
+    System.Console.WriteLine();
+}
+
+void Parity(int[] Numbers)
+{
+    int sum = 0;
+    foreach (int number in Numbers)
+        if (number % 2 == 0)
+            sum++;
+    System.Console.WriteLine($"Количество чётных элементов = {sum}");
+}
