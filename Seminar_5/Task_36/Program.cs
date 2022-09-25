4a0d8857d615d763778efa5adf428affeec1aaa4
Alexander <xanderx2201@yandex.ru>
2022-09-25 17:53:47 +0300

    Homework seminar_5 complete

---
+++ Seminar_5/Task_36/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+int size = 0;
+System.Console.WriteLine("Введите размерность массива");
+size = int.Parse(Console.ReadLine()!);
+
+int[] Numbers = new int[size];
+
+Fillarray(Numbers);
+Printarray(Numbers);
+Parity(Numbers);
+
+void Fillarray(int[] Numbers)
+{
+    for (int i = 0; i < size; i++)
+        Numbers[i] = new Random().Next(-50, 100);
+}
+
+void Printarray(int[] Numbers)
+{
+    for (int i = 0; i < size; i++)
+        Console.Write($"{Numbers[i]} ");
+    System.Console.WriteLine();
+}
+
+void Parity(int[] Numbers)
+{
+    int sum = 0;
+    for (int i = 1; i < Numbers.Length; i += 2)
+        sum += Numbers[i];
+    System.Console.WriteLine($"Сумма нечётных элементов = {sum}");
+}
---
+++ Seminar_5/Task_38/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+//[3 7 22 2 78] -> 76
+
+
+System.Console.WriteLine("Введите размерность массива");
+int size = int.Parse(Console.ReadLine()!);
+
+double[] Numbers = new double[size];
+
+Fillarray(Numbers);
+Printarray(Numbers);
+double Max = Maximum(Numbers);
+double Min = Minimum(Numbers);
+System.Console.WriteLine($"Max {Max} - Min {Min} = {Max - Min}");
+
+
+void Fillarray(double[] Numbers)
+{
+    for (int i = 0; i < size; i++)
+        Numbers[i] = new Random().Next(1, 100);
+}
+
+void Printarray(double[] Numbers)
+{
+    for (int i = 0; i < size; i++)
+        Console.Write($"{Numbers[i]} ");
+    System.Console.WriteLine();
+}
+
+double Maximum(double[] Numbers)
+{
+    Max = Numbers[0];
+    for (int i = 1; i < size; i++)
+        if (Numbers[i] >= Max)
+            Max = Numbers[i];
+    return Max;
+}
+
+double Minimum(double[] Numbers)
+{
+    Min = Numbers[0];
+    for (int i = 1; i < size; i++)
+        if (Numbers[i] <= Min)
+            Min = Numbers[i];
+    return Min;
+}
