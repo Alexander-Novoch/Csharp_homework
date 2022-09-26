ee77504edb9266d36399a19ae26339e8d42cb4ab
Alexander <xanderx2201@yandex.ru>
2022-09-26 20:50:47 +0300

    Homework seminar_6 complete

---
+++ Seminar_6/Task_41/Program.cs
@@ -0,0 +1,49 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+// так как в задании не указано точно как вводить числа, то я решил схитрить и заставить 
+//пользователя немного поработать))
+
+System.Console.WriteLine("Сколько чисел вы хотите ввести??");
+System.Console.Write("M = ");
+int size = int.Parse(Console.ReadLine()!);
+
+//мне кажется что эта программа без функций будет немного короче и проще))
+// int[] Numbers = new int[size];
+// int PositiveNumbers = 0;
+
+// for (int i = 0; i < size; i++)
+// {
+//     System.Console.WriteLine($"Введите {i + 1} число и после нажмите Enter");
+//     Numbers[i] = int.Parse(Console.ReadLine()!);
+//     if (Numbers[i] > 0)
+//         PositiveNumbers++;
+// }
+//Console.WriteLine($"Колличество введенных чисел > 0 = {PositiveNumbers}");
+
+int[] Numbers = CreateAndFillArray(size);
+PositiveNumbers(Numbers);
+
+
+int[] CreateAndFillArray(int size)
+{
+    int[] Numbers = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        System.Console.WriteLine($"Введите {i + 1} число и после нажмите Enter");
+        Numbers[i] = int.Parse(Console.ReadLine()!);
+    }
+    return Numbers;
+}
+
+void PositiveNumbers(int[] Numbers)
+{
+    int sum = 0;
+    foreach (int number in Numbers)
+    {
+        if(number > 0)
+            sum++;
+    }
+    Console.WriteLine($"Колличество введенных чисел > 0 = {sum}");
+}
---
+++ Seminar_6/Task_43/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+System.Console.WriteLine("y = k1 * x + b1");
+System.Console.WriteLine("y = k2 * x + b2");
+string[] arguments = { "b1", "k1", "b2", "k2" };
+
+System.Console.WriteLine("Введите аргументы уравнений для расчёта точки пересечения");
+
+double[] arg = FillArray(arguments);
+//само уравнение я решил вне программы
+double pointX = (arg[2] - arg[0]) / (arg[1] - arg[3]); // x = ( b2 - b1 ) / ( k1 - k2 )
+double pointY = arg[1] * pointX + arg[0]; // y = k1 * x + b1     (хотя можно проверять по любому из двух уравнений данных в условии)
+
+System.Console.WriteLine($"Коорджинаты точки пересечения = ({pointX} ; {pointY})");
+
+double[] FillArray(string[] arguments)
+{
+    double[] arg = new double[arguments.Length];
+    for (int i = 0; i < arguments.Length; i++)
+    {
+        Console.Write($"Введите {arguments[i]} = ");
+        arg[i] = double.Parse(Console.ReadLine()!);
+    }
+    return arg;
+}
